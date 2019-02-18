74fe350b3076f2b19d897f82204084001cc0b8cc
Daan Snijders <daan.97@hotmail.com>
2019-02-18 16:30:31 +0100

    Update Definitions.py

--- Definitions.py
+++ Definitions.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import math
 
+ha = 0.161
+Ca = 0.505
+n = 11
+
 def beambending(ray,rby,rcy,rdy,q,dx,moi,centroid):
 
     print('test')
@@ -10,14 +14,20 @@ def centroid():
     
     print ('Test')
     
-    
-def Boom_spacing(ha, Ca, n):
+  
+#ha = 0.161 m, Ca = 0.505 and n = 11
+def boom_spacing(ha, Ca, n):
     Cr = Ca-ha/2.
     Ct = math.sqrt(Cr**2+(ha/2)**2)
     Cb = Ct
     Cc = math.pi*ha
     Circ = Ct+Cb+Cc
-    Spacing = Circ/n
-    print ('The spacing is', Spacing)
+    spacing = Circ/n
+   
     
-    return Spacing
+    return spacing
+
+def boom_placement():
+    spacing = boom_spacing(ha, Ca, n)
+    
+    
