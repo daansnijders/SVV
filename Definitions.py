090638c33fbab765d7841c844578d9e93bc3cd8b
Daan Snijders <daan.97@hotmail.com>
2019-02-18 14:42:45 +0100

    Definitions

---
+++ Definitions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 14:42:25 2019
+
+@author: Daan
+"""
+
---
+++ Main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 11:55:02 2019
+
+@author: Daan
+"""
+
+print('hello world')
+
+#test4567890123
