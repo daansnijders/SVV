8d82cc581aa67f32f22bacabe606f26470b799a7
SybrenBootsma <47575390+SybrenBootsma@users.noreply.github.com>
2019-02-20 16:21:40 +0100

    Update Forces.py

--- Forces.py
+++ Forces.py
@@ -9,7 +9,7 @@ from math import *
 
 def ReactionForces(theta,P,q,Ca,ha,E,Ixx,Iyy,x1,x2,x3,xa,span,d1,d3): 
     """ Reaction Forces in x direction """
-    Rx2 = 0.
+    Rx2 = 0.                                    #sum of forces in x
     
     """ Reaction Forces in y direction """
     
@@ -25,9 +25,7 @@ def ReactionForces(theta,P,q,Ca,ha,E,Ixx,Iyy,x1,x2,x3,xa,span,d1,d3):
     ans4 = [-q/24.*(x2**4.)]
     ans5 = [E*Ixx*d3 - q/24.* x3**4.]
     
-    
     A = np.array(eq1, eq2, eq3, eq4, eq5)
-    
     b = np.array(ans1,ans2,ans3,ans4,ans5)
     # x = [R1y, R2y, R3y, A, B], where A and B are the integration constants from the bending equation
     x = np.linalg.solve(A, b)
@@ -35,10 +33,10 @@ def ReactionForces(theta,P,q,Ca,ha,E,Ixx,Iyy,x1,x2,x3,xa,span,d1,d3):
     #check solution
     check = np.allclose(np.dot(A, x), b)
     
+    #Get reaction forces from x
     Ry1 = x[0]
     Ry2 = x[1]
     Ry3 = x[2]
-    
     
     """ Reaction Forces in z direction """
     A1 = -span*q/tan(theta) - P                #force in actuator 1 (sum of moments around hinge)
@@ -54,16 +52,15 @@ def ReactionForces(theta,P,q,Ca,ha,E,Ixx,Iyy,x1,x2,x3,xa,span,d1,d3):
     ans4 = [-A1/6.*((xa/2.)**3.)]
     ans5 = [-A1/6.*(x3-x2+(xa/2.))**3. -P/6.*(x3-x2-(xa/2.))**3.]
     
-    
     A = np.array(eq1, eq2, eq3, eq4, eq5)
-    
     b = np.array(ans1,ans2,ans3,ans4,ans5)
-    # x = [R1y, R2y, R3y, A, B], where A and B are the integration constants from the bending equation
+    # y = [R1z, R2z, R3z, A, B], where A and B are the integration constants from the bending equation
     y = np.linalg.solve(A, b)
     
     #check solution
     check = np.allclose(np.dot(A, y), b)
     
+    #Get reaction forces from y
     Rz1 = y[0]
     Rz2 = y[1]
     Rz3 = y[2]
@@ -79,3 +76,5 @@ def Torsion(theta,P,q,Ca,ha):
     
     M_add_P = P*movey
     M_add_q = q*movex
+    
+
