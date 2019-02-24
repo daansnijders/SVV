348e76ed6bf564f8c302ac970decfac002febd22
Daan Snijders <daan.97@hotmail.com>
2019-02-24 16:17:10 +0100

    Update Validation_program.py

--- Validation_program.py
+++ Validation_program.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 24 15:32:56 2019
+import scipy
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 
-@author: Daan
-"""
-
+node = []
+j = 0
+with open("./data/F100-19.inp") as g:
+    for oneline in g:
+        if oneline.startswith('*') == False:
+            if j < 3244:
+                j =+ 1
+                line_cont = [str(y) for y in oneline.split(',')]
+                node.append(line_cont)
+          
+            
+        else:
+            j =+ 1
