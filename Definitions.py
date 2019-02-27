e89a8fc6bde05622a78b0eda7ea6c34887fc3129
pered <47572304+pered@users.noreply.github.com>
2019-02-27 19:57:34 +0100

    Update Definitions.py

--- Definitions.py
+++ Definitions.py
@@ -1361,8 +1361,8 @@ def centroidglobal(zcg, theta):
 
 
 def booms():
-    tring_booms = [1,2,3,4,12,13,8,9,10,11]
-    circ_booms = [12,5,6,7,13]
+    tring_booms = [12,13,8,9,10,11,1,2,3,4]
+    circ_booms = [13,12,5,6,7]
     return tring_booms, circ_booms
 
 def ratetwistandshearflowdiscretisation(t_skin, t_spar, spacing, l1,l2,l3,l4,xa, Mz, My, Mx, Vy, Vz, I, stiff_area, zcg, nodepos, dist, arc, Ca, ha, G, theta, alpharad, n):
